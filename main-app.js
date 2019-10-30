292e8a8aee43649609f34c93a1c21d579df28aaf
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-10-30 12:02:14 -0400

    promt changed

--- main-app.js
+++ main-app.js
@@ -1,5 +1,5 @@
 function clickFunction(){
-let input = prompt("Please enter your city and the temperature type");
+let input = prompt("Please enter your city and temperature unit");
 input = input.split(' ')
 let result = weather(input[0],input[1])
 document.getElementById("result").innerText = result
